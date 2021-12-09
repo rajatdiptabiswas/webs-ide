6c66e3080a675268857b5245968531a1021acd61
Rajat Dipta Biswas <rdbiswas97@gmail.com>
2021-12-09 22:35:58 +0600

    Update Navbar
    - Added default solid background color to navbar
    - Added props to control the background color and text color

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,19 @@
-export default function Navbar() {
+type NavbarProps = {
+  textColor?: string;
+  backgroundColor?: string;
+};
+
+export default function Navbar({ textColor, backgroundColor }: NavbarProps) {
   return (
-    <nav className="inset-x-0 top-0 p-4 sm:px-8 h-16 shadow-lg fixed z-10">
+    <nav
+      className="inset-x-0 top-0 p-4 sm:px-8 h-16 shadow-lg fixed z-10 bg-gray-50"
+      style={{ backgroundColor: backgroundColor }}
+    >
       <div className="flex items-center justify-center">
-        <div className="flex text-lg font-mono sm:text-2xl">
+        <div
+          className="flex text-lg font-mono sm:text-2xl"
+          style={{ color: textColor }}
+        >
           <span className="font-light">webs</span>
           <span className="font-bold">IDE</span>
         </div>
